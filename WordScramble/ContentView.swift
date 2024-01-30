036b343bad7c90e6cdeea4e7eacf6b736dbf50ad
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-30 15:47:05 +0300

    Work with strings.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -31,6 +31,30 @@ struct ContentView: View {
             }
         }
     }
+    
+    func testStrings() {
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+        
+        let input2 = """
+                    a
+                    b
+                    c
+                    """
+        let letters2 = input2.components(separatedBy: "\n")
+        
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let missspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = missspelledRange.location == NSNotFound
+    }
 }
 
 #Preview {
